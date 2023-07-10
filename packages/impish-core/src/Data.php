4cada43a54694beebaa4fddd96cd3664e356b70d
marchalbersma <contact@marchalbersma.com>
2023-07-10 23:38:53 +0200

    Add array wrapper creation from array

--- packages/impish-core/src/Data.php
+++ packages/impish-core/src/Data.php
@@ -10,12 +10,19 @@ namespace Impish\Core;
 class Data
 {
     /**
+     * Creates a new wrapper around the given array.
+     *
+     * @param array $array The underlying array.
+     */
+    public function __construct(protected array $array = []) {}
+
+    /**
      * Returns the underlying array.
      *
      * @return array The underlying array.
      */
     public function all(): array
     {
-        return [];
+        return $this->array;
     }
 }
--- packages/impish-core/tests/Unit/DataTest.php
+++ packages/impish-core/tests/Unit/DataTest.php
@@ -9,3 +9,12 @@ it('can be created empty', function () {
 
     expect($data->all())->toBe([]);
 });
+
+it('can be created from array', function (array $array) {
+    $data = new Data($array);
+
+    expect($data->all())->toBe($array);
+})->with([
+    'indexed'     => [['The Fellowship of the Ring', 'The Two Towers', 'The Return of the King']],
+    'associative' => [['series' => 'The Lord of the Rings', 'director' => 'Peter Jackson']],
+]);
